b58eb81162d5d5e12fefd4910ec3e518348de3e0
Dylan Thomas <dylanthomas1108@gmail.com>
2023-02-22 23:38:47 +0000

    started work on macros calculator

--- Year1/Sem2/a00307281/jscripts/JSFile.js
+++ Year1/Sem2/a00307281/jscripts/JSFile.js
@@ -1,14 +1,25 @@
+// TODO
+// Figure out how to decipher which button is selected
+// Fix the buttons in HTML
+// Macro calculator for rest of males and all females
+// BMI calculator
+// Body fat calculator
+
+var weight = 0;
+var height = 0;
+var age = 0;
+
 var $ = function (id) {
   return document.getElementById(id);
 };
-var ResetTextfields = function () {
+function ResetTextfields() {
   $("width").value = "";
   $("length").value = "";
   $("height").value = "";
   $("radius").value = "";
   $("resultPara").innerHTML =
     "Volume is the quantity of three-dimensional space enclosed by some closed boundary, for example, the space that a substance (solid, liquid, gas, or plasma) or shape occupies or contains";
-};
+}
 window.onload = function () {
   $("calculate").onclick = function () {
     calculate();
@@ -17,95 +28,119 @@ window.onload = function () {
     ResetTextfields();
   };
 };
-var SetFields = function (id) {
-  /*This function takes an id which is an integer as a parameter
-	This is parameter is passed from the HTML file when a choice is click on. 
-	If the id is equal to 1 then the radius field is set to disabled and the 
-	image src is changed to show the cube.jpg file
-	If the id is 2 then the appropriate fileds is disabled and the cylinder.jpg image is 
-	displayed. If the id is 3, agian the appropriate fields aer disabled/enabled and the sphere.jpg 
-	image is displayed.*/
-  if (id == 1) {
-    $("width").disabled = false;
-    $("length").disabled = false;
-    $("height").disabled = false;
-    $("radius").disabled = true;
-    $("theImage").src = "images/cube.jpg";
+
+function SetFields(id) {
+  /*This function takes an id which is an integer as a parameter.
+	This is parameter is passed from the radio buttons in the HTML file.
+  e.g If the id is 1, then the Macros calculator is shown*/
+}
+
+function calculateMacros() {
+  /*This function takes the gender, weight, age, height and activity values from the HTML.
+	It then calculates the persons BMR, calorie count and macro split
+	The results are shown on the right hand side*/
+  var littleNoneActivity = 1.2;
+  var lightActivity = 1.375;
+  var moderateActivity = 1.55;
+  var veryActivity = 1.725;
+  var extremelyActivity = 1.9;
+  var BMR = 0;
+  var carbs = 0;
+  var protein = 0;
+  var fats = 0;
+
+  weight = weightValue;
+  height = heightValue;
+  age = ageValue;
+
+  if ("Male is Selected") {
+    BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+    console.log("Base BMR = " + BMR);
+
+    // little - no activity
+    if ("little - none is selected") {
+      BMR = BMR * littleNoneActivity;
+      console.log("Daily BMR =" + BMR);
+      if ("Lose weight is selected") {
+        console.log("Calculating macro split for male, little activity, weight loss");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.4) / 4);
+        fats = Math.ceil((BMR * 0.2) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+      if ("Maintain weight is selected") {
+        console.log("Calculating macro split for male, little activity, maintain weight");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.3) / 4);
+        fats = Math.ceil((BMR * 0.3) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+      if ("Gain weight is selected") {
+        console.log("Calculating macro split for male, little activity, gain weight");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.3) / 4);
+        fats = Math.ceil((BMR * 0.3) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+    }
+
+    // light activity
+    if ("light activity is selected") {
+      BMR = BMR * lightActivity;
+      console.log("Daily BMR =" + BMR);
+      if ("Lose weight is selected") {
+        console.log("Calculating macro split for male, light activity, weight loss");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.4) / 4);
+        fats = Math.ceil((BMR * 0.2) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+      if ("Maintain weight is selected") {
+        console.log("Calculating macro split for male, light activity, maintain weight");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.3) / 4);
+        fats = Math.ceil((BMR * 0.3) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+      if ("Gain weight is selected") {
+        console.log("Calculating macro split for male, light activity, gain weight");
+        carbs = Math.ceil((BMR * 0.4) / 4);
+        protein = Math.ceil((BMR * 0.3) / 4);
+        fats = Math.ceil((BMR * 0.3) / 9);
+
+        $("resultPara").innerHTML = "carbs: " + carbs + " protein: " + protein + " fats: " + fats;
+      }
+    }
   }
-  if (id == 2) {
-    $("width").disabled = true;
-    $("length").disabled = true;
-    $("height").disabled = false;
-    $("radius").disabled = false;
-    $("theImage").src = "images/cylinder.jpg";
-  }
-  if (id == 3) {
-    $("width").disabled = true;
-    $("length").disabled = true;
-    $("height").disabled = true;
-    $("radius").disabled = false;
-    $("theImage").src = "images/sphere.jpg";
-  }
-};
-var calculateTank = function () {
-  /*This function takes the width, height and length values from the HTML, 
-	checks to see if they are numbers, if so it calculates the volume of the tank
-	The results are printed to the paragraph at the bottom of the screen, as seen in
-	the screen shot*/
-  var l = parseFloat($("length").value);
-  var w = parseFloat($("width").value);
-  var h = parseFloat($("height").value);
-  if (isNaN(l) || isNaN(w) || isNaN(h)) {
-    console.log("Did not enter numbers");
-    alert("You must enter numerical values!");
-  } else {
-    console.log("Calculating volume of a tank");
-    let volume = (l * w * h).toFixed(2);
-    $("resultPara").innerHTML = "The volume of this tank is: " + volume + "cm<sup>3</sup>";
-  }
-};
-var calculateCylinder = function () {
-  /*This function takes the radius and height values from the HTML, 
-	checks to see if they are numbers, if so it calculates the volume of the cylinder
-	The results are printed to the paragraph at the bottom of the screen, as seen in
-	the screen shot*/
-  var r = parseFloat($("radius").value);
-  var h = parseFloat($("height").value);
-  if (isNaN(r) || isNaN(h)) {
-    console.log("Did not enter numbers");
-    alert("You must enter numerical values!");
-  } else {
-    console.log("Calculating volume of a cylinder");
-    let volume = (Math.PI * Math.pow(r, 2) * h).toFixed(2);
-    $("resultPara").innerHTML = "The volume of this cylinder is: " + volume + "cm<sup>3</sup>";
-  }
-};
-var calculateSphere = function () {
-  /*This function takes the radius value from the HTML, 
-	checks to see if it is a numbers, if so it calculates the volume of the sphere
-	The result are printed to the paragraph at the bottom of the screen, as seen in
-	the screen shot*/
-  var r = parseFloat($("radius").value);
-  if (isNaN(r)) {
-    console.log("Did not enter numbers");
-    alert("You must enter numerical values!");
-  } else {
-    console.log("Calculating volume of a sphere");
-    let volume = ((4 * Math.PI * Math.pow(r, 3)) / 3).toFixed(2);
-    $("resultPara").innerHTML = "The volume of this sphere is: " + volume + "cm<sup>3</sup>";
-  }
-};
-var calculate = function () {
+}
+function calculateBMI() {
+  /* NEED TO ADD COMMENTS */
+}
+function calculateBodyFat() {
+  /* NEED TO ADD COMMENTS */
+}
+function calculate() {
   /*This function checks to see what radio-button is selected and then
-	calls the appropriate function.  For example if the Tank is checked the calculateTank
+	calls the appropriate function.  For example if the Macro Calculator is checked the calculateMacros
 	function is called.*/
-  if ($("tank").checked == true) {
-    calculateTank();
-  }
-  if ($("cylinder").checked == true) {
-    calculateCylinder();
-  }
-  if ($("sphere").checked == true) {
-    calculateSphere();
-  }
-};
+}
+
+function showAgeVal(ageValue) {
+  console.log(ageValue);
+  $("ageTag").innerHTML = "I am " + ageValue + " years old";
+}
+
+function showHeightVal(heightValue) {
+  console.log(heightValue);
+  $("heightTag").innerHTML = "I am " + heightValue + "cm tall";
+}
+
+function showWeightVal(weightValue) {
+  console.log(weightValue);
+  $("weightTag").innerHTML = "I am " + weightValue + "kg";
+}
